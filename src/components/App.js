93172f53c2f5c5a67424f922f9f6e983907935a7
IainLR <iainrobertzon@gmail.com>
2020-10-30 16:13:15 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -53,7 +53,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App " >
         <Nav handleFilter ={this.handleFilter}/>
         <PigDisplay hogs = {this.state.hogs} hideThatPig = {this.hideThatPig} hiddenHogs = {this.state.hiddenHogs} />
       </div>
